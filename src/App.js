3d48ee2cdc2867d4750456c3703c6d16f3fe95d1
AmberLilith <amber.lilith.ordone@gmail.com>
2023-05-18 23:06:34 -0300

    first_commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="mx-2 mt-2">
+      <div className='row'>
+        <div className='col'>
+        <button className='btn btn-warning'>CRIAR USUÁRIO</button>
+        </div>
+        <div className='col'>
+        <h1 className='text-center'>LOGIN MANAGER</h1>
+        </div>
+        <div className='col'>
+        <button className='btn btn-warning position-absolute end-0'>LOGOUT</button>
+        </div>
+      </div>      
     </div>
   );
 }
